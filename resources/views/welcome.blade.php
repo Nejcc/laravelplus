fbcdd43af2b26eabbb99b8c4bcd805a6de536e3a
Neo <enginesilver@gmail.com>
2023-02-14 22:22:21 +0100

    - fix issues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,20 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title>{{ config('app.name') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet"/>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body  class=" d-flex flex-column" id="app">
+<body class=" d-flex flex-column" id="app">
 <div class="page page-center bg-dark">
     <div class="container container-tight py-4">
-{{--        <div class="text-center mb-4">--}}
-{{--            <a href="." class="navbar-brand navbar-brand-autodark"><img src="{{ asset('static/logo.svg') }}" height="36" alt=""></a>--}}
-{{--        </div>--}}
+        {{--        <div class="text-center mb-4">--}}
+        {{--            <a href="." class="navbar-brand navbar-brand-autodark"><img src="{{ asset('static/logo.svg') }}" height="36" alt=""></a>--}}
+        {{--        </div>--}}
         <div class="text-center">
             <div class="my-5">
                 <h1 class="text-danger">Laravel<b class="text-primary">Plus</b></h1>
@@ -25,7 +26,7 @@
         </div>
     </div>
 </div>
-<script src="{{ asset('dist/js/tabler.min.js') }}" defer></script>
-<script src="{{ asset('dist/js/demo.min.js') }}" defer></script>
+{{--<script src="{{ asset('dist/js/tabler.min.js') }}" defer></script>--}}
+{{--<script src="{{ asset('dist/js/demo.min.js') }}" defer></script>--}}
 </body>
 </html>
