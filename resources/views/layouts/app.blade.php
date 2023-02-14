d28824c16b8e0dc90a118e2628b946fc2f439f4b
Neo <enginesilver@gmail.com>
2023-02-14 22:11:42 +0100

    - add theme for UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     ])
 </head>
 <body>
-    <div id="app">
+    <div id="">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
