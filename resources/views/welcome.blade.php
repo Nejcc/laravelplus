4c9b2101703c0f0796d5cc4086b4bdf47d952fbf
Neo <enginesilver@gmail.com>
2023-02-14 22:18:55 +0100

    - add theme for UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <title>{{ config('app.name') }}</title>
-    <link href="{{ asset('dist/css/tabler.min.css?1674944402') }}" rel="stylesheet"/>
-{{--    <link href="{{ asset('dist/css/tabler-flags.min.css?1674944402') }}" rel="stylesheet"/>--}}
-{{--    <link href="{{ asset('dist/css/tabler-payments.min.css?1674944402') }}" rel="stylesheet"/>--}}
-{{--    <link href="{{ asset('dist/css/tabler-vendors.min.css?1674944402') }}" rel="stylesheet"/>--}}
-{{--    <link href="{{ asset('dist/css/demo.min.css?1674944402') }}" rel="stylesheet"/>--}}
+    <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet"/>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body  class=" d-flex flex-column" id="app">
-<script src="{{ asset('dist/js/demo-theme.min.js?1674944402') }}"></script>
-<div class="page page-center">
+<div class="page page-center bg-dark">
     <div class="container container-tight py-4">
 {{--        <div class="text-center mb-4">--}}
 {{--            <a href="." class="navbar-brand navbar-brand-autodark"><img src="{{ asset('static/logo.svg') }}" height="36" alt=""></a>--}}
@@ -30,7 +25,7 @@
         </div>
     </div>
 </div>
-{{--<script src="{{ asset('dist/js/tabler.min.js?1674944402') }}" defer></script>--}}
-{{--<script src="{{ asset('dist/js/demo.min.js?1674944402') }}" defer></script>--}}
+<script src="{{ asset('dist/js/tabler.min.js') }}" defer></script>
+<script src="{{ asset('dist/js/demo.min.js') }}" defer></script>
 </body>
 </html>
