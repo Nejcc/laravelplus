ce188d563c998e02289e91d778504223bd04a9ad
Neo <enginesilver@gmail.com>
2023-02-14 21:58:35 +0100

    - add theme for UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,10 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite([
+    'resources/sass/app.scss',
+    'resources/js/app.js'
+    ])
 </head>
 <body>
     <div id="app">
