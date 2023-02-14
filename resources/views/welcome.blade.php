c77192068ce574a06dc32fe40f4836dec3284fda
Neo <enginesilver@gmail.com>
2023-02-14 22:53:07 +0100

    - add sitemap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,14 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>{{ config('app.name') }}</title>
+    <title>{{ config('app.name') }} - The Ultimate Laravel Missing Backend</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="LaravelPlus is the missing Laravel backend that provides a prepared structure to work with, saving you time and effort in developing web applications or websites with Laravel. With pre-built components and modules, LaravelPlus makes it easy to customize and integrate functionality into your project, making it the ultimate Laravel backend framework.">
+    <meta name="keywords" content="LaravelPlus, Laravel Administration backend framework,Laravel Admin, Laravel components, web application development">
+    <meta property="og:title" content="LaravelPlus - The Ultimate Laravel Backend Framework">
+    <meta property="og:description" content="LaravelPlus is the missing Laravel backend that provides a prepared structure to work with, saving you time and effort in developing web applications or websites with Laravel.">
+    <meta property="og:image" content="https://example.com/images/laravelplus-logo.png">
+    <meta property="og:url" content="https://laravelplus.com/">
     <link href="{{ asset('dist/css/tabler.min.css') }}" rel="stylesheet"/>
 {{--    @vite(['resources/sass/app.scss', 'resources/js/app.js'])--}}
     <!-- Google tag (gtag.js) -->
