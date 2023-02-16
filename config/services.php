6f87af61ee75c60cf45d63c2120de2b8208104ed
Neo <enginesilver@gmail.com>
2023-02-16 22:02:46 +0100

    - sociolite added

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://example.com/callback-url',
+        'redirect' => route('sociolite.github.callback'),
     ],
 
 ];
