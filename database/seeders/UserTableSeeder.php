707b156d4385bca5301f2c650940c387e02f0180
Neo <enginesilver@gmail.com>
2023-02-16 20:34:35 +0100

    - add user updates

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Database\Seeders;
+
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+
+class UserTableSeeder extends Seeder
+{
+    private $roles = ['admin', 'editor', 'writer', 'translator', 'power user', 'user'];
+    private $permissionsOnGroup = [
+        'admin' => [
+            'view'   => [
+                'user',
+                'sitemap',
+                'role',
+                'permission',
+            ],
+            'create' => [
+                'permission',
+                'sitemap',
+                'user',
+                'role',
+            ],
+            'update' => [
+                'permission',
+                'sitemap',
+                'user',
+                'role',
+            ],
+            'delete' => [
+                'permission',
+                'sitemap',
+                'user',
+                'role',
+
+            ],
+        ],
+        'user'  => [
+        ]
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->generateAllRoles();
+        $this->generateAllUsers();
+    }
+
+    /**
+     * @return void
+     */
+    private function generateAllRoles(): void
+    {
+        $god = Role::create(['name' => 'super-admin']);
+
+        foreach ($this->roles as $role) {
+            $newRole = Role::create(['name' => $role]);
+            if (!empty($this->permissionsOnGroup[$role])) {
+                foreach ($this->permissionsOnGroup[$role] as $key => $permission) {
+                    if (!empty($permission[0])) {
+                        foreach ($permission as $p) {
+                            $newPermission = Permission::updateOrCreate(['name' => "{$key} {$p}", 'group_name' => $p]);
+                            $newRole->givePermissionTo($newPermission);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function GenerateAllUsers(): void
+    {
+        $users = [
+            ['name' => 'Nejc', 'username' => 'nejc', 'email' => null, 'password' => null, 'role' => 'super-admin'],
+            ['name' => 'Martin', 'username' => 'martin', 'email' => null, 'password' => null, 'role' => 'user'],
+        ];
+
+        foreach ($users as $user) {
+            $user_object = User::create([
+                'name'     => $user['name'],
+                'email'    => $this->generateUserEmailAddress($user),
+                'password' => $this->generateUserPassword($user),
+            ]);
+
+            $user_object->assignRole($user['role']);
+        }
+    }
+
+    private function generateUserEmailAddress($user)
+    {
+        if ($user['email'] === null) {
+            return $user['username'] . '@' . config('main_domain_name', config('app.domain'));
+        }
+
+        return $user['email'];
+    }
+
+    private function generateUserPassword($user)
+    {
+        if ($user['password'] == null) {
+            return bcrypt($user['username'] . config('auth.password_default'));
+        }
+
+        return bcrypt($user['password']);
+    }
+}
