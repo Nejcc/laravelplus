8e7ce8aca0696b57eb195c174f6a9951fa8659dd
Neo <enginesilver@gmail.com>
2023-02-16 20:29:47 +0100

    - other fixes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/tos', [App\Http\Controllers\HomeController::class, 'index'])->name('tos');
