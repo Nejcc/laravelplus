7cc47d403f77e6bc8e60fd5d090890a331480a86
Neo <enginesilver@gmail.com>
2023-02-16 22:03:49 +0100

    - sociolite added

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => route('sociolite.github.callback'),
+        'redirect' => 'https://laravelplus.com/login/github/callback',
     ],
 
 ];
