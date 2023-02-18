de8db3b8726a98965302f71e39477dc3667ea9eb
Neo <enginesilver@gmail.com>
2023-02-18 11:59:56 +0100

    - update files - add switch user remote

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,19 +48,21 @@
                     <div class="btn-list overide-dark">
 {{--                        <span class="mt-2">:</span>--}}
                         @if(!session()->has('main_user_id'))
-                            <form action="{{ route('admin.switch-user.login-as') }}" method="post" class="d-flex">
-                                @csrf
-                                <select name="switch_user_to" class="form-select ">
-                                    <option selected disabled>Switch User</option>
-                                    @foreach(\App\Models\User::all() as $user)
-                                        @if($user->id != auth()->id())
-                                            <option value="{{ $user->id }}">{{ ucfirst($user->username ?: $user->email) }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                                <input type="hidden" value="{{ auth()->id() }}" name="main_user_id">
-                                <button class="btn btn-link border-radius-none">Switch</button>
-                            </form>
+                            @role('super-admin|admin')
+                                <form action="{{ route('admin.switch-user.login-as') }}" method="post" class="d-flex">
+                                    @csrf
+                                    <select name="switch_user_to" class="form-select ">
+                                        <option selected disabled>Switch User</option>
+                                        @foreach(\App\Models\User::all() as $user)
+                                            @if($user->id != auth()->id())
+                                                <option value="{{ $user->id }}">{{ ucfirst($user->username ?: $user->email) }}</option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                    <input type="hidden" value="{{ auth()->id() }}" name="main_user_id">
+                                    <button class="btn btn-link border-radius-none">Switch</button>
+                                </form>
+                            @endrole
                         @else
                             <form action="{{ route('switch-user.back') }}" method="post" class="d-flex">
                                 @csrf
