d0cca5ce04bf38b84c098f4c40613fbbbadd356e
nejcc <nejc.cotic@gmail.com>
2023-04-17 20:57:16 +0200

    - wip dynamic user notification

---
+++ app/Events/NewModelCreated.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use App\Notifications\NewUserNotification;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+// TODO: wip
+class NewModelCreated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('channel-name'),
+        ];
+    }
+
+    public function handle()
+    {
+        $user = User::find($this->model->user_id);
+        $user->notify(new NewUserNotification($this->model));
+    }
+}
