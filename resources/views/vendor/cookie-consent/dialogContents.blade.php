7d05ec9e4b7577192b1a173880cdffb957d76767
nejcc <nejc.cotic@gmail.com>
2023-05-16 20:19:49 +0200

    - update pint

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -1,4 +1,4 @@
-<div class="offcanvas offcanvas-bottom h-auto show" tabindex="-1" id="offcanvasBottom" aria-modal="true" role="dialog">
+<div class="offcanvas offcanvas-bottom h-auto show js-cookie-consent" tabindex="-1" id="offcanvasBottom" aria-modal="true" role="dialog">
     <div class="offcanvas-body">
         <div class="container">
             <div class="row align-items-center">
@@ -11,7 +11,7 @@
                     </button>
                 </div>
                 <div class="col-auto">
-                    <button type="button" class="btn btn-primary" data-bs-dismiss="offcanvas">
+                    <button type="button" class="btn btn-primary js-cookie-consent-agree" data-bs-dismiss="offcanvas">
                         {{ __('cookie-consent::texts.agree') }}
                     </button>
                 </div>
