8f93de4ef8368ec534d76f37314fec6ef2737558
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-10-28 15:53:53 -0500

    Update UserFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,6 +6,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -20,12 +22,12 @@ final class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name'              => fake()->name(),
-            'email'             => fake()->unique()->safeEmail(),
+            'name'              => $this->faker->name(),
+            'email'             => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password'          => Hash::make('password'),
             'remember_token'    => Str::random(10),
-            'is_read_news'      => 0,
+            'is_read_news'      => 0, // Todo: move to separate table
         ];
     }
 
@@ -39,5 +41,14 @@ final class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => now(),
+        ])->has(
+            Role::where('name', 'admin')->first()
+        );
     }
 }
