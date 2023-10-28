675ee8dd5f4f4797fa84d9037857eee3bb4e2d6d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-10-28 10:28:13 -0500

    Update EventServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Listeners\ModelCreatedListener;
+use App\Events\ModelCreated;
 
 final class EventServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,9 @@ final class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        ModelCreated::class => [
+            ModelCreatedListener::class
+        ]
     ];
 
     /**
