527926d327faa1e1cf44522278b37295c606fdba
neo <info@after.si>
2025-04-15 19:09:04 +0200

    chore: update core framework files for Laravel 12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
             Route::middleware('web', 'auth')
                 ->prefix('admin')
-            
+
                 ->group(base_path('routes/admin.php'));
         },
     )
