a28c7a57b158414c6ea62eab48eb733d89feb20f
neo <info@after.si>
2025-04-15 20:30:56 +0200

    refactor: update views and layouts for better organization

--- resources/views/components/header/navigation.blade.php
+++ resources/views/components/header/navigation.blade.php
@@ -1,137 +1,81 @@
+@props(['active' => false])
+
 <div>
     <div class="collapse navbar-collapse" id="navbar-menu">
         <div class="navbar navbar-light">
             <div class="container-xl">
                 <ul class="navbar-nav">
-                    <li class="nav-item {{ (request()->is('home') ? 'active' : '') }}">
-                        <a class="nav-link" href="{{ route('home') }}">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                           stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                           stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path
-                                  d="M5 12l-2 0l9 -9l9 9l-2 0"/><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/><path
-                                  d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/></svg>
-                    </span>
-                            <span class="nav-link-title">
-                      {{ __('Home') }}
-                    </span>
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
-                           data-bs-auto-close="outside" role="button" aria-expanded="false">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                           stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                           stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path
-                                  d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5"/><path d="M12 12l8 -4.5"/><path
-                                  d="M12 12l0 9"/><path d="M12 12l-8 -4.5"/><path d="M16 5.25l-8 4.5"/></svg>
-                    </span>
-                            <span class="nav-link-title">
-                      {{ __('submenu' )}}
-                    </span>
-                        </a>
-                        <div class="dropdown-menu">
-                            <div class="dropdown-menu-columns">
-                                <div class="dropdown-menu-column">
-                                    <a class="dropdown-item" href="./empty.html">
-                                        Empty page
-                                    </a>
-                                    <div class="dropend">
-                                        <a class="dropdown-item dropdown-toggle" href="#sidebar-cards"
-                                           data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button"
-                                           aria-expanded="false">
-                                            Cards
-                                            <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
-                                        </a>
-                                        <div class="dropdown-menu">
-                                            <a href="./cards.html" class="dropdown-item">
-                                                Sample cards
-                                            </a>
-                                            <a href="./card-actions.html" class="dropdown-item">
-                                                Card actions
-                                                <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">New</span>
-                                            </a>
-                                            <a href="./cards-masonry.html" class="dropdown-item">
-                                                Cards Masonry
-                                            </a>
+                    @foreach(config('navigation.items') as $item)
+                        @php
+                            $hasRoles = isset($item['roles']);
+                            $hasItems = isset($item['items']);
+                            $isActive = isset($item['active']) ? request()->is($item['active']) : false;
+                        @endphp
+
+                        @if(!$hasRoles || ($hasRoles && auth()->user()->hasAnyRole($item['roles'])))
+                            <li class="nav-item {{ $isActive ? 'active' : '' }} {{ $hasItems ? 'dropdown' : '' }}">
+                                <a class="nav-link {{ $hasItems ? 'dropdown-toggle' : '' }}" 
+                                   href="{{ $hasItems ? '#' : route($item['route'] ?? '#') }}"
+                                   @if($hasItems)
+                                       data-bs-toggle="dropdown"
+                                       data-bs-auto-close="outside"
+                                       role="button"
+                                       aria-expanded="false"
+                                   @endif>
+                                    <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                        <i class="ti ti-{{ $item['icon'] ?? 'circle' }}"></i>
+                                    </span>
+                                    <span class="nav-link-title">
+                                        {{ __($item['title']) }}
+                                    </span>
+                                </a>
+
+                                @if($hasItems)
+                                    <div class="dropdown-menu">
+                                        <div class="dropdown-menu-columns">
+                                            <div class="dropdown-menu-column">
+                                                @foreach($item['items'] as $subItem)
+                                                    @if(isset($subItem['items']))
+                                                        <div class="dropend">
+                                                            <a class="dropdown-item dropdown-toggle" 
+                                                               href="#"
+                                                               data-bs-toggle="dropdown"
+                                                               data-bs-auto-close="outside"
+                                                               role="button"
+                                                               aria-expanded="false">
+                                                                {{ __($subItem['title']) }}
+                                                                @if(isset($subItem['badge']))
+                                                                    <span class="badge badge-sm bg-green-lt text-uppercase ms-auto">
+                                                                        {{ $subItem['badge'] }}
+                                                                    </span>
+                                                                @endif
+                                                            </a>
+                                                            <div class="dropdown-menu">
+                                                                @foreach($subItem['items'] as $nestedItem)
+                                                                    <a href="{{ route($nestedItem['route'] ?? '#') }}" 
+                                                                       class="dropdown-item">
+                                                                        {{ __($nestedItem['title']) }}
+                                                                    </a>
+                                                                @endforeach
+                                                            </div>
+                                                        </div>
+                                                    @else
+                                                        <a class="dropdown-item" 
+                                                           href="{{ route($subItem['route'] ?? '#') }}">
+                                                            @if(isset($subItem['icon']))
+                                                                <i class="ti ti-{{ $subItem['icon'] }} me-2"></i>
+                                                            @endif
+                                                            {{ __($subItem['title']) }}
+                                                        </a>
+                                                    @endif
+                                                @endforeach
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="dropdown-menu-column">
-                                    <a class="dropdown-item" href="./pagination.html">
-                                        <!-- Download SVG icon from http://tabler-icons.io/i/pie-chart -->
-                                        Pagination
-                                    </a>
-                                    <div class="dropend">
-                                        <a class="dropdown-item dropdown-toggle" href="#sidebar-authentication"
-                                           data-bs-toggle="dropdown" data-bs-auto-close="outside" role="button"
-                                           aria-expanded="false">
-                                            Authentication
-                                        </a>
-                                        <div class="dropdown-menu">
-                                            <a href="./sign-in.html" class="dropdown-item">
-                                                Sign in
-                                            </a>
-                                            <a href="./sign-in-link.html" class="dropdown-item">
-                                                Sign in link
-                                            </a>
-                                            <a href="./sign-in-illustration.html" class="dropdown-item">
-                                                Sign in with illustration
-                                            </a>
-                                            <a href="./sign-in-cover.html" class="dropdown-item">
-                                                Sign in with cover
-                                            </a>
-                                            <a href="./sign-up.html" class="dropdown-item">
-                                                Sign up
-                                            </a>
-                                            <a href="./forgot-password.html" class="dropdown-item">
-                                                Forgot password
-                                            </a>
-                                            <a href="./terms-of-service.html" class="dropdown-item">
-                                                Terms of service
-                                            </a>
-                                            <a href="./auth-lock.html" class="dropdown-item">
-                                                Lock screen
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    @role('super-admin|admin')
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#navbar-base" data-bs-toggle="dropdown"
-                           data-bs-auto-close="outside" role="button" aria-expanded="false">
-                    <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/package -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                           stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                           stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path
-                                  d="M12 3l8 4.5l0 9l-8 4.5l-8 -4.5l0 -9l8 -4.5"/><path d="M12 12l8 -4.5"/><path
-                                  d="M12 12l0 9"/><path d="M12 12l-8 -4.5"/><path d="M16 5.25l-8 4.5"/></svg>
-                    </span>
-                            <span class="nav-link-title">
-                      {{ __('Administration' )}}
-                    </span>
-                        </a>
-                        <div class="dropdown-menu">
-                            <div class="dropdown-menu-columns">
-                                <div class="dropdown-menu-column">
-                                    <a class="dropdown-item" href="{{ route('admin.roles.index') }}">
-                                        {{ __('Roles') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('admin.permissions.index') }}">
-                                        {{ __('Permissions') }}
-                                    </a>
-                                    <a class="dropdown-item" href="{{ route('admin.users.index') }}">
-                                        {{ __('Users') }}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    @endrole
+                                @endif
+                            </li>
+                        @endif
+                    @endforeach
                 </ul>
                 <div class="my-2 my-md-0 flex-grow-1 flex-md-grow-0 order-first order-md-last">
                     <form action="./" method="get" autocomplete="off" novalidate>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,90 +1,127 @@
 <!doctype html>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>{{ config('app.name') }} - @yield('title')</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-
+    <!-- CSS files -->
+    <link href="{{ asset(config('app.theme.path').'dist/css/tabler.min.css?1674944402') }}" rel="stylesheet"/>
+    <link href="{{ asset(config('app.theme.path').'dist/css/tabler-flags.min.css?1674944402') }}" rel="stylesheet"/>
+    <link href="{{ asset(config('app.theme.path').'dist/css/tabler-payments.min.css?1674944402') }}" rel="stylesheet"/>
+    <link href="{{ asset(config('app.theme.path').'dist/css/tabler-vendors.min.css?1674944402') }}" rel="stylesheet"/>
+    <link href="{{ asset(config('app.theme.path').'dist/css/demo.min.css?1674944402') }}" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
+        <style>
+            @import url('https://rsms.me/inter/inter.css');
+            :root {
+                --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+            }
+            body {
+                font-feature-settings: "cv03", "cv04", "cv11";
+            }
+        </style>
     <!-- Scripts -->
     @vite(['resources/sass/app.scss','resources/js/app.js'])
+    @stack('css')
 </head>
 <body>
-<div id="">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+<script src="{{ asset(config('app.theme.path').'dist/js/demo-theme.min.js') }}"></script>
+<div class="page" id="app">
+    <!-- Navbar -->
+    <header class="navbar navbar-expand-md d-print-none" data-bs-theme="dark">
+        <div class="container-xl">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu"
+                    aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    <button class="dropdown-item">
-                                        {{ __('Logout') }}
-                                    </button>
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
+            <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
+                <a href="{{ route('home') }}">
+                    <h2 class="mt-2">{{ config('app.name') }}</h2>
+                </a>
+            </h1>
+            <div class="navbar-nav flex-row order-md-last">
+                <div class="nav-item d-none d-md-flex me-3">
+                    <div class="btn-list overide-dark">
+                        <x-utilities.switch-user></x-utilities.switch-user>
+                    </div>
+                </div>
+                <div class="d-none d-md-flex">
+                    <x-locale></x-locale>
+                    <a href="?theme=dark" class="nav-link px-0 mt-1 hide-theme-dark" title="Enable dark mode"
+                       data-bs-toggle="tooltip"
+                       data-bs-placement="bottom">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                             stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                             stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"/>
+                        </svg>
+                    </a>
+                    <a href="?theme=light" class="nav-link px-0 mt-1 hide-theme-light" title="Enable light mode"
+                       data-bs-toggle="tooltip"
+                       data-bs-placement="bottom">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                             stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                             stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"/>
+                            <path
+                                d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"/>
+                        </svg>
+                    </a>
+                    <x-header.notifications></x-header.notifications>
+                </div>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
+                       aria-label="Open user menu">
+                        <span class="avatar avatar-sm" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                        <div class="d-none d-xl-block ps-2">
+                            <div>{{ auth()->user()->email ?? '---' }}</div>
+                            <div class="mt-1 small text-muted">{{ ucfirst(auth()->user()->getRoleNames()[0] ?? '---') }}</div>
+                        </div>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button class="dropdown-item">
+                                <i class="ti ti-logout me-2"></i>
+                                {{ __('Logout') }}
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
-
-    <main class="py-4">
+    </header>
+    <header class="navbar-expand-md">
+        <x-header.navigation></x-header.navigation>
+    </header>
+    <div class="page-wrapper">
+        @yield('header')
         @yield('content')
-    </main>
-</div>
 
+        @if(config('theme.has.footer'))
+            <x-footer.general></x-footer.general>
+        @endif
+    </div>
+    @yield('modals')
+</div>
+<!-- Libs JS -->
+<script src="{{ asset(config('app.theme.path').'dist/libs/nouislider/dist/nouislider.min.js?1674944402') }}"
+        defer></script>
+<script src="{{ asset(config('app.theme.path').'dist/libs/litepicker/dist/litepicker.js?1674944402') }}" defer></script>
+<script src="{{ asset(config('app.theme.path').'dist/libs/tom-select/dist/js/tom-select.base.min.js?1674944402') }}"
+        defer></script>
+<!-- Tabler Core -->
+<script src="{{ asset(config('app.theme.path').'dist/js/tabler.min.js?1674944402') }}" defer></script>
+<script src="{{ asset(config('app.theme.path').'dist/js/demo.min.js?1674944402') }}" defer></script>
+@stack('js')
 <script>
-    window.translations = @json(@$translations);
+    window.translations = @json(session()->get('translations', []));
 </script>
 </body>
 </html>
