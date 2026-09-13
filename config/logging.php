dfdb73c503c38afb3a8045d47d0c67265bfec778
nejcc <nejc.cotic@gmail.com>
2026-09-13 23:47:47 +0200

    logging: dnevni logi (LOG_STACK=daily) namesto enega laravel.log
    single kanal se nikoli ne rotira; daily obdrzi LOG_DAILY_DAYS (14) dni.

--- config/logging.php
+++ config/logging.php
@@ -55,7 +55,7 @@ return [
     'channels' => [
         'stack' => [
             'driver'            => 'stack',
-            'channels'          => ['single'],
+            'channels'          => ['daily'],
             'ignore_exceptions' => false,
         ],
 
